6be2cc6180ff6a788e11e96df2b7e44543887a57
Steven Thomas Lord <stevenlord@me.com>
2019-06-20 20:13:17 +0100

    initial commit

---
+++ Fitness Invaders/GameOverScene.swift
@@ -0,0 +1,75 @@
+//
+//  GameOverScene.swift
+//  Fitness Invaders
+//
+//  Created by Steven Lord on 15/06/2019.
+//  Copyright © 2019 Steven Lord. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameOverScene: SKScene {
+    
+    // Private GameScene Properties
+    
+    var contentCreated = false
+    
+    // Object Lifecycle Management
+    
+    // Scene Setup and Content Creation
+    
+    override func didMove(to view: SKView) {
+        
+        if (!self.contentCreated) {
+            self.createContent()
+            self.contentCreated = true
+        }
+    }
+    
+    func createContent() {
+        
+        let gameOverLabel = SKLabelNode(fontNamed: "Courier")
+        gameOverLabel.fontSize = 50
+        gameOverLabel.fontColor = SKColor.white
+        gameOverLabel.text = "Game Over!"
+        gameOverLabel.position = CGPoint(x: self.size.width/2, y: 2.0 / 3.0 * self.size.height);
+        
+        self.addChild(gameOverLabel)
+        
+        let tapLabel = SKLabelNode(fontNamed: "Courier")
+        tapLabel.fontSize = 25
+        tapLabel.fontColor = SKColor.white
+        tapLabel.text = "(Tap to Play Again)"
+        tapLabel.position = CGPoint(x: self.size.width/2, y: gameOverLabel.frame.origin.y - gameOverLabel.frame.size.height - 40);
+        
+        self.addChild(tapLabel)
+        
+        // black space color
+        self.backgroundColor = SKColor.black
+        
+    }
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)  {
+        
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)  {
+        
+        let gameScene = GameScene(size: self.size)
+        gameScene.scaleMode = .aspectFill
+        
+        self.view?.presentScene(gameScene, transition: SKTransition.doorsCloseHorizontal(withDuration: 1.0))
+        
+    }
+}
--- Fitness Invaders/GameViewController.swift
+++ Fitness Invaders/GameViewController.swift
@@ -15,21 +15,33 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        // Configure the view.
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        /* Sprite Kit applies additional optimizations to improve rendering performance */
+        skView.ignoresSiblingOrder = true
+        
+        // Create and configure the scene.
+        let scene = GameScene(size: skView.frame.size)
+        skView.presentScene(scene)
+        
+//        if let view = self.view as! SKView? {
+//            // Load the SKScene from 'GameScene.sks'
+//            if let scene = SKScene(fileNamed: "GameScene") {
+//                // Set the scale mode to scale to fit the window
+//                scene.scaleMode = .aspectFill
+//
+//                // Present the scene
+//                view.presentScene(scene)
+//            }
+//
+//            view.ignoresSiblingOrder = true
+//
+//            view.showsFPS = true
+//            view.showsNodeCount = true
+//        }
     }
 
     override var shouldAutorotate: Bool {
